21a6530bc95f3283dc9708a9a49c8e2ed20612c2
hoangbinhan <hoangbinhana3@gmail.com>
2020-11-29 21:22:42 +0700

    transfer js to jsx component

---
+++ src/pages/404.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Result, Button } from "antd";
+import Link from "next/link";
+
+export default function error() {
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="Sorry, the page you visited does not exist."
+            extra={
+                <Link href="/">
+                    <Button type="primary">Back Home</Button>
+                </Link>
+            }
+        />
+    );
+}
